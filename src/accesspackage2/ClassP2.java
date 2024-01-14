4f5a2b2f85f081897ec95927cf0d1264dedf8551
MiranIbrahim <moeenuddin.miran@gmail.com>
2024-01-14 13:07:45 +0600

    okkkkkkkkkk

--- src/accesspackage2/ClassP2.java
+++ src/accesspackage2/ClassP2.java
@@ -6,8 +6,12 @@ public class ClassP2 {
 	public static void main(String[] args) {
 		ClassP1 ii = new ClassP1();
 		ClassP2second p2Second = new ClassP2second();
-		p2Second.setA("IT IS A TEST AGENT SCULLY");
+		p2Second.setA("Extraterrestrial life exists Scully");
 		System.out.println(p2Second.getA());
+		
+		ClassP2Third p2third = new ClassP2Third();
+		p2third.setB("I do not believe you Mulder.");
+		System.out.println(p2third.getB());
 	}
 
 }
